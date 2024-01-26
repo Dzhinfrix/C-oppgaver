421b85272d7c5017e23c8d975aa1dc7db1338ccb
Alyona <fairy.albert.hofmann@gmail.com>
2024-01-26 16:55:42 +0100

    added task1

---
+++ oppgaver1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Clear();
+int Main(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int naturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{naturalNumber(n, m + 1)}, ");
+  return m;
+}
+
+int n = Main("Input N: ");
+int m = Main("Input M: ");
+if (n < m)
+{
+  Console.WriteLine("n needs to be bigger then m");
+  return;
+}
+Console.WriteLine(naturalNumber(n, 1));
