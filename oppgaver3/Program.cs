835ab715c3ddb7424b62cf3dda8e5d34454928c1
Alyona <fairy.albert.hofmann@gmail.com>
2024-01-26 16:59:14 +0100

    add task3

---
+++ oppgaver3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] myArray = { 1, 2, 3, 4, 5 };
+        PrintArrayReversed(myArray, myArray.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
+}
